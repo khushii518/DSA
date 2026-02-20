12d6d670dc466f133e625f2e860de3c02dcbedec
khushii518 <khushikori04@gmail.com>
2026-02-20 22:27:33 +0530

    Add files via upload

---
+++ day20.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+int compare(const void* a, const void* b) {
+long long x = *(long long*)a;
+long long y = *(long long*)b;
+return (x > y) - (x < y);
+}
+int main() {
+int n;
+scanf("%d", &n);
+int arr[n];
+for (int i = 0; i < n; i++) {
+scanf("%d", &arr[i]);
+}
+long long prefix[n + 1];
+prefix[0] = 0;
+for (int i = 1; i <= n; i++) {
+prefix[i] = prefix[i - 1] + arr[i - 1];
+}
+qsort(prefix, n + 1, sizeof(long long), compare);
+long long count = 0;
+long long freq = 1;
+for (int i = 1; i <= n; i++) {
+if (prefix[i] == prefix[i - 1]) {
+freq++;
+} else {
+count += freq * (freq - 1) / 2;
+freq = 1;
+}
+}
+count += freq * (freq - 1) / 2;
+printf("%lld\n", count);
+return 0;
+}
