70c7b2fe41df96de52ead89f77013c000b396916
khushii518 <khushikori04@gmail.com>
2026-03-20 08:37:38 +0530

    Add files via upload

---
+++ day48.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct TreeNode {
+    int val;
+    struct TreeNode* left;
+    struct TreeNode* right;
+};
+
+// Create new node
+struct TreeNode* newNode(int val) {
+    struct TreeNode* node = (struct TreeNode*)malloc(sizeof(struct TreeNode));
+    node->val = val;
+    node->left = node->right = NULL;
+    return node;
+}
+
+// Count leaf nodes
+int countLeaves(struct TreeNode* root) {
+    if (root == NULL) return 0;
+
+    // If leaf node
+    if (root->left == NULL && root->right == NULL)
+        return 1;
+
+    return countLeaves(root->left) + countLeaves(root->right);
+}
